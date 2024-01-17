bbbbb39989e2c2ab1052d7c4c0de70d46734e6f4
robertkelm <kelm@rackow-berlin.de>
2024-01-17 16:25:08 +0100

    Init default modal

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -25,7 +25,21 @@ const MODAL_BUTTON_SUBMIT = 'modalButtonSubmit' // for the modal's submit button
 
 <template>
     <div :id="MODAL_ID" class="modal" tabindex="-1">
-        <!-- TODO: implement -->
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title">Modal title</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <p>Modal body text goes here.</p>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+          </div>
+        </div>
+      </div>
     </div>
 </template>
 
