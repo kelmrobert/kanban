f1f56fdef8da84cd34557360853b62ae213c026f
robertkelm <kelm@rackow-berlin.de>
2024-02-07 19:50:50 +0100

    6.1.1 Prepare data

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,17 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const path = require('path')
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 
 ///////////////////////////
 // Data initialization
 ///////////////////////////
-const columns = require('./data/columns.json')
-const tags = require('./data/tags.json')
+const columns = require('./data/columns.json');
+const tags = require('./data/tags.json');
+
+let taskIdCounter = 1;
+taskIdCounter = columns.forEach(column => {
+    taskIdCounter += column.tasks.length;
+});
 
 //TODO: implement (see 6.1.1)
 
