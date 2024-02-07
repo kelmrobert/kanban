b31d3f36b474b76b4ea1f3342fc82556b40d6155
robertkelm <kelm@rackow-berlin.de>
2024-02-07 20:19:30 +0100

    Init server

--- backend/index.js
+++ backend/index.js
@@ -24,6 +24,12 @@ columns.forEach(column => {
 const app = express();
 const port = 3000;
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html')) // TODO correct?
+});
+
+app.listen(port, () => console.log(`Express Server ist listening on port ${port}...`));
+
 
 ///////////////////////////
 // CRUD operations
